b1f8bdfd729244351a3f0875639e49ca627ac686
Zhang Wenhao <wh01.zhang@outlook.com>
2025-07-16 23:30:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/WIPES',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
